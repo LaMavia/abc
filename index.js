591747e93cfc84b2fd7f207e3249b8072227d1a9
LastShadowPL <xeloxpl@windowslive.com>
2017-11-03 20:13:02 +0100

    started lvls

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 console.log('https://www.youtube.com/watch?v=Cvt98tMVbHk');
+var factorial = function (n) {
+    if (n === 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+};
 var rand = function (max, min) {
     if (min === void 0) { min = 0; }
     return Math.floor(Math.random() * (max - min) + min);
@@ -16,13 +22,40 @@ var randLetters = [];
 var handleClick = function (e) {
     this;
     if (this.innerHTML === rLetter)
-        console.log('proper'), reset();
+        console.log('proper'), reset(), points++;
     else
         this.disabled = true;
 };
+var points = 0;
+var lvl = points + 2 * 1;
+var updateLvl = function (toAdd) {
+    if (toAdd === void 0) { toAdd = 0; }
+    lvl = points + 2 + toAdd;
+};
 var pads = document.querySelectorAll('.pad');
 pads.forEach(function (pad) { return pad.addEventListener('click', handleClick); });
+var initPad = function (amount) {
+    var outPoint = document.querySelector('.game');
+    for (var i = 0; i < amount; i++) {
+        var newPad = document.createElement('button');
+        newPad.classList.add('pad');
+        outPoint.appendChild(newPad);
+        newPad.addEventListener('click', handleClick);
+        pads.forEach(function (p) {
+            p.removeEventListener('click', handleClick);
+        });
+        pads = document.querySelectorAll('.pad');
+        pads.forEach(function (p) {
+            p.addEventListener('click', handleClick);
+        });
+    }
+    console.log(pads.length);
+};
 var reset = function () {
+    updateLvl();
+    if (!(lvl % 6))
+        initPad(1);
+    document.body.style.setProperty('--pads-amount', pads.length);
     var isInArr = function (item, arr) {
         var found = false, i = 0;
         while (!found && i < arr.length) {
@@ -32,7 +65,7 @@ var reset = function () {
         return found;
     };
     randLetters = [];
-    while (randLetters.length < 3) {
+    while (randLetters.length < pads.length) {
         var r = rand(letters.length);
         if (!isInArr(letters[r], randLetters))
             randLetters.push(letters[r]);
@@ -41,7 +74,7 @@ var reset = function () {
         p.innerHTML = randLetters[i];
         p.disabled = false;
     });
-    rLetter = randLetters[rand(3)];
+    rLetter = randLetters[rand(pads.length)];
     var speech = new SpeechSynthesisUtterance(rLetter);
     window.speechSynthesis.speak(speech);
 };
--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 console.log('https://www.youtube.com/watch?v=Cvt98tMVbHk')
+const factorial = (n) => {
+if(n===0) return 1
+else return n * factorial(n - 1)
+}
 const rand = (max: number, min: number = 0): number => {
   return Math.floor(Math.random() * (max - min) + min)
 }
@@ -7,7 +11,7 @@ const rgb = () => {
   document.body.style.setProperty('--rgb', color)
 }
 rgb()
-setInterval(rgb,2000)
+setInterval(rgb, 2000)
 const defColor = '#ffffff',
   wrColor = '#ff0000',
   corColor = '#00ff50'
@@ -17,14 +21,38 @@ let rLetter = 'c'
 let randLetters = []
 const handleClick = function(e: any): void {
   (this as HTMLElement)
-  if (this.innerHTML === rLetter) console.log('proper'), reset()
+  if (this.innerHTML === rLetter) console.log('proper'), reset(), points++
   else this.disabled = true
 }
+let points = 0
+let lvl = points + 2 * 1
+const updateLvl = (toAdd: number = 0) => {
+  lvl = points + 2 + toAdd
+}
+let pads = document.querySelectorAll('.pad') as any
 
-const pads = document.querySelectorAll('.pad') as any
 pads.forEach(pad => pad.addEventListener('click', handleClick))
-
+const initPad = (amount: number) => {
+  let outPoint = document.querySelector('.game')
+  for (let i = 0; i < amount; i++) {
+    let newPad = document.createElement('button')
+    newPad.classList.add('pad')
+    outPoint.appendChild(newPad)
+    newPad.addEventListener('click', handleClick)
+    pads.forEach(p => {
+      p.removeEventListener('click', handleClick)
+    })
+    pads = document.querySelectorAll('.pad')
+    pads.forEach(p => {
+      p.addEventListener('click', handleClick)
+    })
+  }
+  console.log(pads.length)
+}
 const reset = (): void => {
+  updateLvl()
+  if(!(lvl % 6)) initPad(1)
+  document.body.style.setProperty('--pads-amount',pads.length)
   const isInArr = (item, arr: any[]) => {
     let found = false,
       i = 0
@@ -35,7 +63,7 @@ const reset = (): void => {
     return found
   }
   randLetters = []
-  while (randLetters.length < 3) {
+  while (randLetters.length < pads.length) {
     let r = rand(letters.length)
     if (!isInArr(letters[r], randLetters)) randLetters.push(letters[r])
   }
@@ -43,7 +71,7 @@ const reset = (): void => {
     p.innerHTML = randLetters[i]
     p.disabled = false
   })
-  rLetter = randLetters[rand(3)]
+  rLetter = randLetters[rand(pads.length)]
   const speech = new SpeechSynthesisUtterance(rLetter)
   window.speechSynthesis.speak(speech)
 }
